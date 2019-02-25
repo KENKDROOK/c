f172a33a05517b748877c2045a95bc7b3347f227
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-02-25 16:16:33 +0530

    Update input constriant.c

--- input constriant.c
+++ input constriant.c
@@ -2,29 +2,27 @@
 
 void main()
 {
-int n,arr[50],i,temp,j;
+int n,ar[50],i,temp,j;
 printf("enter the value");
 scanf("%d",&n);
 for(i=0;i<n;i++)
 {
- scanf("%d",&arr[i]);
+ scanf("%d",&ar[i]);
 }
 for(i=0;i<n;i++)
 {
  for(j=i+1;j<n;j++)
  {
-   if(arr[i]>arr[j])
+   if(arr[i]>ar[j])
     {
-        temp=arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
+        temp=ar[i];
+        ar[i]=ar[j];
+        ar[j]=temp;
      }
   }
 }
 for(j=0;j<n;j++)
 {
-    printf("%d",arr[j]);
-    
-}
-//getch();
+    printf("%d",ar[j]);
+    }
 }
