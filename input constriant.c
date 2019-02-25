4c0c0debcced012640358fb6276e82aeeda46964
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-02-25 16:14:16 +0530

    Create input constriant.c

---
+++ input constriant.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void main()
+{
+int n,arr[50],i,temp,j;
+printf("enter the value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+ scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+ for(j=i+1;j<n;j++)
+ {
+   if(arr[i]>arr[j])
+    {
+        temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+     }
+  }
+}
+for(j=0;j<n;j++)
+{
+    printf("%d",arr[j]);
+    
+}
+//getch();
+}
