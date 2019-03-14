df192001952f3bc338211dc25a3442ced20b80be
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-03-14 16:21:28 +0530

    Create count the numeric character.c

---
+++ count the numeric character.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,c=0,l,g;
+    char str[50];
+    printf("enter the value");
+    gets(str);
+    l=strlen(str);
+    for(i=0;i<l;i++)
+    {
+        if(isdigit(str[i]))
+        //if(str[i]=' ')
+        {
+              c++;
+        }
+        //printf("%d",c);
+    }
+   printf("%d",c);
+}
