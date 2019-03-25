afcc09bc5dc1674edb536ba330253a6c0868be6e
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-03-25 19:51:20 +0530

    Create without using strcat.c

---
+++ without using strcat.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+    char str[50],str1[50];
+    printf("enter the string1:");
+    scanf("%s",str);
+    printf("enter the string2:");
+    scanf("%s",str1);
+    printf("%s%s",str,str1);
+    }
