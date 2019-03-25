d56c0b0637d2e920aa9766064915987e3ed4eaf3
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-03-25 20:01:11 +0530

    Create range [1,10].c

---
+++ range [1,10].c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    printf("Enter the number ");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 2 ...9:
+        printf("YES");
+        break;
+        default:
+        printf("NO");
+        break;
+    }
+        
+}
+        
