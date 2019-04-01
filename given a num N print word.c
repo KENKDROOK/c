35ec8b862aaf528b938764744c558a79cf89f2f0
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-04-01 15:34:05 +0530

    Create given a num N print word.c

---
+++ given a num N print word.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    printf("enter the value");
+    scanf("%d",&n);
+    switch(n)
+    {
+        case 1:
+        {
+            printf("one");
+            break;
+        }
+        case 2:
+        {
+            printf("two");
+            break;
+        }
+        case 3:
+        {
+            printf("three");
+            break;
+        }
+        case 4:
+        {
+            printf("four");
+            break;
+        }
+        case 5:
+        {
+            printf("one");
+            break;
+        }
+        case 6:
+        {
+            printf("one");
+            break;
+        }
+        case 7:
+        {
+            printf("seven");
+            break;
+        }
+        case 8:
+        {
+            printf("eight");
+            break;
+        }
+        case 9:
+        {
+            printf("nine");
+            break;
+        }
+        case 10:
+        {
+            printf("ten");
+            break;
+        }
+        
+    }
+    getch();
+}
