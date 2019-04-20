ace7188b447b95897aebe8b037cd802486804fb4
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-04-20 11:41:13 +0530

    Create check whether if it is prime .c

---
+++ check whether if it is prime .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n;
+	int i,flag=0;
+	scanf("%d",&n);
+	for(i=2;i<=n/2;i++)
+	{
+		if(n%i == 0)
+        {
+            flag = 1;
+            break;
+        }
+	}
+	if (flag == 0)
+          printf("yes");
+        else
+          printf("no");
+}
