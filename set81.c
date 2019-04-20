7ce5719a59769076de5b3963eecfead37a560291
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-04-20 21:07:08 +0530

    Create set81.c

---
+++ set81.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a,b,c,d,df,df1;
+   scanf("%d %d",&a,&b);
+   scanf("%d %d",&c,&d);
+   if(a>b)
+   {
+       df=a-b;
+   }
+   else
+   {
+       df=b-a;
+   }
+   if(c>d)
+   {
+       df1=c-d;
+   }
+   else
+   {
+       df1=d-c;
+   }
+   printf("%d\n%d",df,df1);
+}
