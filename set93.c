58124595ec1c830604a8b4426a665566cda6f691
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-04-20 21:17:37 +0530

    Create set93.c

---
+++ set93.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ long int a,c,d;
+ char b;
+ scanf("%ld %c %ld",&a,&b,&c);
+ if(b==37)
+ {
+     d=a%c;
+ }
+ if(b==47)
+ {
+     d=a/c;
+ }
+ printf("%ld",d);
+ return 0;
+}
