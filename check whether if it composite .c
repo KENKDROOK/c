5e495a2a89946355cf6ff11d0553271125f62b8c
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-04-20 20:59:45 +0530

    Create check whether if it composite .c

---
+++ check whether if it composite .c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,f=0,i;
+    scanf("%d",&n);
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            f=1;
+            break;
+        }
+    }
+    if(f==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+   
