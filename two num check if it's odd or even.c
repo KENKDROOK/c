f5d2a6ad42b96a5840acee4a45ab676f48ec2854
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-04-20 10:52:48 +0530

    Create two num check if it's odd or even.c

---
+++ two num check if it's odd or even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    if((num1+num2)%2==0)
+    {
+        printf("even");
+        
+    }
+        else
+        {
+            printf("odd");
+        }
+}
