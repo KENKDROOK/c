90fcf377db61e029a60e5f52b9f1ec6b7abe895e
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-05-19 12:14:08 +0530

    Update set117.c

--- set117.c
+++ set117.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    a=(a*b)/c;
-    printf("%d",a);
+    int p,k,d;
+    scanf("%d %d %d",&p,&k,&d);
+    p=(p*k)/d;
+    printf("%d",p);
     return 0;
 }
