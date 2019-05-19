a96b9b39737cff23780bb7ea6e472b2a5ff1a231
KENKDROOK <45662125+KENKDROOK@users.noreply.github.com>
2019-05-19 11:54:23 +0530

    Update set105.c

--- set105.c
+++ set105.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int p,t,r;
-float si;
-scanf("%d %d %d",&p,&t,&r);
-si=p*t*r/100;
-printf("%1.f",floor(si));
+int s,t,r;
+float k;
+scanf("%d %d %d",&s,&t,&r);
+k=s*t*r/100;
+printf("%1.f",floor(k));
     return 0;
 }
